9c1993fbb643da569acd4533e1f406fa349a53a1
Xbera25 <69676812+Xbera25@users.noreply.github.com>
2022-09-25 16:07:33 +0400

    module_1_solution

---
+++ module_1.py
@@ -0,0 +1,18 @@
+import random
+
+if __name__ == '__main__':
+    start = 0
+    end = 1000
+    n = 100
+    my_list = [random.randint(start, end) for _ in range(n)]
+
+for i in range(len(my_list)):
+    for j in range(i + 1, len(my_list)):
+        if my_list[i] > my_list[j]:
+            my_list[i], my_list[j] = my_list[j], my_list[i]
+
+even_list = [x for x in my_list if x%2==0]
+print(sum(even_list)/len(even_list))
+
+odd_list = [x for x in my_list if x%2!=0]
+print(sum(odd_list)/len(odd_list))
