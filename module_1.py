63c32376590345a5a89606fcadb6f27e96ad9258
Xbera25 <69676812+Xbera25@users.noreply.github.com>
2022-10-10 17:00:00 +0400

    comments edited

--- module_1.py
+++ module_1.py
@@ -4,15 +4,20 @@ if __name__ == '__main__':
     start = 0
     end = 1000
     n = 100
+
+    # create list of 100 random numbers from 0 to 1000
     my_list = [random.randint(start, end) for _ in range(n)]
 
+    # sort list from min to max (without using sort())
     for i in range(len(my_list)):
         for j in range(i + 1, len(my_list)):
             if my_list[i] > my_list[j]:
                 my_list[i], my_list[j] = my_list[j], my_list[i]
 
+    # calculate average for even and odd numbers
     even_list = [x for x in my_list if x % 2 == 0]
-    print(sum(even_list)/len(even_list))
-
     odd_list = [x for x in my_list if x % 2 != 0]
+
+    # print both average result in console
+    print(sum(even_list) / len(even_list))
     print(sum(odd_list)/len(odd_list))
--- module_2.py
+++ module_2.py
@@ -2,30 +2,38 @@ import random
 import string
 
 if __name__ == '__main__':
+    # create a list of random number of dicts (from 2 to 10)
     n = random.randint(2, 10)
     my_list = []
+
     for _ in range(n):
-        random_letter = random.choice(string.ascii_letters).lower()
-        random_int = random.randint(0, 100)
-        my_list.append({random_letter: random_int})
+        tiny_dict = {}
+        random_letters = random.sample(string.ascii_lowercase, random.randint(1, 5))
+        for random_letter in random_letters:
+            tiny_dict[random_letter] = random.randint(0, 100)
+        my_list.append(tiny_dict)
 
     print(my_list)
 
+    # collect all values per key
     value_collection = {}
-    result = {}
     for idx, mini_dict in enumerate(my_list):
-        key = list(mini_dict.keys())[0]
-        if key not in value_collection:
-            value_collection[key] = [mini_dict[key]]
-        else:
-            value_collection[key].append(mini_dict[key])
+        for key in list(mini_dict.keys()):
+            if key not in list(value_collection.keys()):
+                value_collection[key] = {idx: mini_dict[key]}
+            else:
+                value_collection[key][idx] = mini_dict[key]
+    # print(value_collection)
 
+    # if more than one key pick the biggest and update key name
+    result = {}
     for key in value_collection.keys():
-        if len(value_collection[key]) == 1:
-            result[key] = value_collection[key][0]
+        index_values_dict = value_collection[key]
+        if len(index_values_dict) == 1:
+            result[key] = list(index_values_dict.values())[0]
         else:
-            max_value = max(value_collection[key])
-            new_key = my_list.index({key:max_value})
+            new_key = max(index_values_dict, key=index_values_dict.get)
+            max_value = index_values_dict[new_key]
             new_key = f'{key}_{new_key+1}'
             result[new_key] = max_value
 
